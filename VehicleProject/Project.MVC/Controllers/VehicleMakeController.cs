4bfb9288c2677fb954a9f5024553b6f0d97354d0
kdjaic <23015083+kdjaic@users.noreply.github.com>
2020-03-09 13:56:11 +0100

    Add files via upload

---
+++ VehicleProject/Project.MVC/Controllers/VehicleMakeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project.Service;
+using Project.Service.Model;
+
+namespace Project.MVC.Controllers
+{
+    public abstract class VehicleMakeController : Controller
+    {
+        private IVehicleMake _vehiclemake;
+
+        public VehicleMakeController(IVehicleMake vehiclemake)
+        {
+            _vehiclemake = vehiclemake;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(IVehicleMake model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Update(int Id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Update(IVehicleMake model)
+        {
+                return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            return View();
+        }
+       /* [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            return View();
+        }*/
+    }
+}
---
+++ VehicleProject/Project.MVC/Controllers/VehicleModelController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project.Service;
+using Project.Service.Service;
+
+namespace Project.MVC.Controllers
+{
+    public abstract class VehicleModelController : Controller
+    {
+        private IVehicleModel _vehiclemodel;
+
+        public VehicleModelController(IVehicleModel vehiclemodel)
+        {
+            _vehiclemodel = vehiclemodel;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(IVehicleModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Update(int Id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Update(IVehicleModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            return View();
+        }
+        /*[HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            return View();
+        }*/
+    }
+}
